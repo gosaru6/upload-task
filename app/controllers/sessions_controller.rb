3c10e7bc73a002e6ea3681195c50952af10cf786
ryota <siroisagani@gmail.com>
2017-10-13 14:27:32 +0900

    修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to user_path(user.id)
+      redirect_to boards_path(user.id)
     else
       flash[:danger] = "ログインに失敗しました"
       render 'new'
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
   def destroy
     session.delete(:user_id)
     flash[:notice] = 'ログアウトしました'
-    redirect_to new_session_path    
+    redirect_to new_session_path
   end
 end
